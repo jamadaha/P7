258048846131e60b3cb3cfd03a741d1edfe12be9
Kristian Skov Johansen <kris701kj@gmail.com>
2022-09-26 08:54:48 +0200

    Added some debug text, to make it clear when the program stops

--- src/P7.cpp
+++ src/P7.cpp
@@ -8,19 +8,26 @@ using namespace std;
 
 int main()
 {
+	cout << "Parsing PDDL files...\n";
 	PDDLDriver driver;
 	driver.parse("Data/gripper.pddl");
 	driver.parse("Data/gripper-4.pddl");
+	cout << "Done!\n";
 
+	cout << "Generating PDDL files...\n";
 	PDDLCodeGenerator pddlGenerator;
 	pddlGenerator.GenerateAndVerifyCode(&(driver), "Data/gripper.pddl", "Data/newDomain.pddl", "Data/gripper-4.pddl", "Data/newProblem.pddl");
+	cout << "Done!\n";
 
-
+	cout << "Parsing SAS file...\n";
 	SASParser parser;
 	auto a = parser.Parse("Data/test_sas_plan");
+	cout << "Done!\n";
 
+	cout << "Generating SAS file...\n";
 	SASCodeGenerator sasGenerator;
 	sasGenerator.GenerateCode(a, "Data/new_sas_plan");
+	cout << "Done!\n";
 
 	return 0;
 }

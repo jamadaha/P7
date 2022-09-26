0136e395af94b2b257c27698ceb3459275ef2414
Dovy <djasulaitis05@gmail.com>
2022-09-26 13:28:51 +0200

    DownwardRunner: add files

---
+++ src/DownwardRunner/DownwardRunner.h
@@ -0,0 +1,13 @@
+#ifndef DOWNWARDRUNNER
+#define DOWNWARDRUNNER
+
+#include <string>
+#include "../Config/config.h"
+
+using namespace std;
+
+struct DownwardRunner {
+    static void runDownward(Config c);
+};
+
+#endif

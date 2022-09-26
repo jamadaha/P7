77728dcd6d00fc008f3b4b010adf25d57c0bbe36
Kristian Skov Johansen <kris701kj@gmail.com>
2022-09-26 09:41:35 +0200

    Merge fixes

--- src/P7.cpp
+++ src/P7.cpp
@@ -9,6 +9,8 @@ using namespace std;
 
 int main()
 {
+	FileVerifier verifier;
+
 	cout << "Parsing PDDL files...\n";
 	PDDLDriver driver;
 	driver.parse("Data/gripper.pddl");
@@ -17,17 +19,30 @@ int main()
 
 	cout << "Generating PDDL files...\n";
 	PDDLCodeGenerator pddlGenerator;
-	pddlGenerator.GenerateAndVerifyCode(&(driver), "Data/gripper.pddl", "Data/newDomain.pddl", "Data/gripper-4.pddl", "Data/newProblem.pddl");
+	pddlGenerator.GenerateCode(&(driver), "Data/newDomain.pddl", "Data/newProblem.pddl");
 	cout << "Done!\n";
 
-	PDDLCodeGenerator generator;
-	generator.GenerateCode(&(driver), "Data/newDomain.pddl", "Data/newProblem.pddl");
-
-	FileVerifier verifier;
+	cout << "Verifying PDDL files...\n";
 	if (!verifier.VerifyFiles("Data/gripper.pddl", "Data/newDomain.pddl"))
 		throw invalid_argument("Files not the same!");
 	if (!verifier.VerifyFiles("Data/gripper-4.pddl", "Data/newProblem.pddl"))
 		throw invalid_argument("Files not the same!");
+	cout << "Done!\n";
+
+	cout << "Parsing SAS file...\n";
+	SASParser sasParser;
+	Plan sasPlan = sasParser.Parse("Data/test_sas_plan");
+	cout << "Done!\n";
+
+	cout << "Generating SAS file...\n";
+	SASCodeGenerator sasGenerator;
+	sasGenerator.GenerateCode(sasPlan, "Data/new_sas_plan");
+	cout << "Done!\n";
+
+	cout << "Verifying SAS file...\n";
+	if (!verifier.VerifyFiles("Data/test_sas_plan", "Data/new_sas_plan"))
+		throw invalid_argument("Files not the same!");
+	cout << "Done!\n";
 
 	return 0;
 }

1816b977970c8bbea92c99c5df87240c070b0a94
Kristian Skov Johansen <kris701kj@gmail.com>
2022-09-30 12:02:26 +0200

    * Added an "impossible" version of gripper, for testing * Fixed return code from the `DownwardRunner`

--- src/DownwardRunner/DownwardRunner.cpp
+++ src/DownwardRunner/DownwardRunner.cpp
@@ -16,7 +16,7 @@ DownwardRunner::DownwardRunnerResult DownwardRunner::RunDownward(Config config, 
 		(istreambuf_iterator<char>()));
 	if (content.find("Solution found.") != string::npos)
 		return DownwardRunner::DownwardRunnerResult::FoundPlan;
-	if (content.find("Plan failed to execute") != string::npos)
+	if (content.find("Search stopped without finding a solution.") != string::npos)
 		return DownwardRunner::DownwardRunnerResult::DidNotFindPlan;
 	return DownwardRunner::DownwardRunnerResult::None;
 }

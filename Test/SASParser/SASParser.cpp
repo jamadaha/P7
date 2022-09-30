7fd4a5ed40f2e2f563fff7b65f8075bdcea63298
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2022-09-30 08:41:29 +0200

    Adds more SASParser test

--- Test/SASParser/SASParser.cpp
+++ Test/SASParser/SASParser.cpp
@@ -3,7 +3,9 @@
 
 #include "../../src/SASParser/SASParser.h"
 
-TEST_CASE("SASParser Base") {
+
+
+TEST_CASE("SASParser SingleAction") {
     SASParser sasParser = SASParser();
     std::string testCase = "(test_action)\n; cost = 1 (general cost)";
     SASPlan plan = sasParser.Parse(testCase);
@@ -13,7 +15,7 @@ TEST_CASE("SASParser Base") {
     REQUIRE(0 == plan.actions[0].parameters.size());
 }
 
-TEST_CASE("SASParser Base_TrailingSpace") {
+TEST_CASE("SASParser SingleAction_TrailingSpace") {
     SASParser sasParser = SASParser();
     std::string testCase = "(test_action) \n; cost = 1 (general cost) ";
     SASPlan plan = sasParser.Parse(testCase);
@@ -23,7 +25,7 @@ TEST_CASE("SASParser Base_TrailingSpace") {
     REQUIRE(0 == plan.actions[0].parameters.size());
 }
 
-TEST_CASE("SASParser Base_PrecedingSpace") {
+TEST_CASE("SASParser SingleAction_PrecedingSpace") {
     SASParser sasParser = SASParser();
     std::string testCase = " (test_action)\n ; cost = 1 (general cost)";
     SASPlan plan = sasParser.Parse(testCase);
@@ -32,4 +34,45 @@ TEST_CASE("SASParser Base_PrecedingSpace") {
     REQUIRE("test_action" == plan.actions[0].name);
     REQUIRE(0 == plan.actions[0].parameters.size());
 }
+
+TEST_CASE("SASParser SingleAction_Parameters") {
+    SASParser sasParser = SASParser();
+    std::string testCase = "(test_action a b)\n ; cost = 1 (general cost)";
+    SASPlan plan = sasParser.Parse(testCase);
+    REQUIRE(1 == plan.cost);
+    REQUIRE(1 == plan.actions.size());
+    REQUIRE("test_action" == plan.actions[0].name);
+    REQUIRE(2 == plan.actions[0].parameters.size());
+    REQUIRE("a" == plan.actions[0].parameters[0]);
+    REQUIRE("b" == plan.actions[0].parameters[1]);
+}
+
+TEST_CASE("SASParser MultiAction") {
+    SASParser sasParser = SASParser();
+    std::string testCase = "(test_action1)\n(test_action2)\n ; cost = 1 (general cost)";
+    SASPlan plan = sasParser.Parse(testCase);
+    REQUIRE(1 == plan.cost);
+    REQUIRE(2 == plan.actions.size());
+    REQUIRE("test_action1" == plan.actions[0].name);
+    REQUIRE("test_action2" == plan.actions[1].name);
+    REQUIRE(0 == plan.actions[0].parameters.size());
+    REQUIRE(0 == plan.actions[1].parameters.size());
+}
+
+TEST_CASE("SASParser MultiAction_Parameters") {
+    SASParser sasParser = SASParser();
+    std::string testCase = "(test_action1 a b)\n(test_action2 c d)\n ; cost = 1 (general cost)";
+    SASPlan plan = sasParser.Parse(testCase);
+    REQUIRE(1 == plan.cost);
+    REQUIRE(2 == plan.actions.size());
+    REQUIRE("test_action1" == plan.actions[0].name);
+    REQUIRE("test_action2" == plan.actions[1].name);
+    REQUIRE(2 == plan.actions[0].parameters.size());
+    REQUIRE(2 == plan.actions[1].parameters.size());
+    REQUIRE("a" == plan.actions[0].parameters[0]);
+    REQUIRE("b" == plan.actions[0].parameters[1]);
+    REQUIRE("c" == plan.actions[1].parameters[0]);
+    REQUIRE("d" == plan.actions[1].parameters[1]);
+}
+
 

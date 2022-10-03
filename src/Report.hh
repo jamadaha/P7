47d8c8882d15df45948557d042f28a9d2091387b
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2022-10-03 10:01:31 +0200

    Generates time report

--- src/Report.hh
+++ src/Report.hh
@@ -5,6 +5,11 @@
 #include <vector>
 #include <chrono>
 
+enum class TimeScale {
+    ms,
+    ns
+};
+
 struct ReportStep {
     std::string desc;
     // How long the step took in nano seconds
@@ -23,16 +28,40 @@ public:
         steps[steps.size() - 1].iTime = std::chrono::steady_clock::now();
         return steps.size() - 1;
     }
+    
     // Returns time taken
-    int64_t Stop() {
-        return Stop(steps.size() - 1);
+    // Default is in ms
+    int64_t Stop(TimeScale ts = TimeScale::ms) {
+        return Stop(steps.size() - 1, ts);
     }
 
-    int64_t Stop(int i) {
+    // Returns time taken
+    // Default is in ms
+    int64_t Stop(int i, TimeScale ts = TimeScale::ms) {
         steps[i].eTime = std::chrono::steady_clock::now();
         steps[i].time = std::chrono::duration_cast<std::chrono::nanoseconds>(steps[i].eTime - steps[i].iTime).count();
-        return steps[i].time / 1000000;
+
+        if (ts == TimeScale::ns)
+            return steps[i].time;
+        else
+            return steps[i].time / 1000000;
     }
+
+    void Print(TimeScale ts = TimeScale::ms) {
+        int lDesc = 0;
+        for (auto step : steps)
+            lDesc = std::max(lDesc, (int) step.desc.size());
+        printf("---- Time Taken ----\n");
+        std::string tts = (ts == TimeScale::ns) ? "(ns)" : "(ms)";
+        printf("%-*s %s %s\n", lDesc, "Description", "Time Taken", tts.c_str());
+        for (auto step : steps) {
+            if (ts == TimeScale::ns)
+                printf("%-*s %ld\n", lDesc, step.desc.c_str(), (long) step.time);
+            else
+                printf("%-*s %f\n", lDesc, step.desc.c_str(), step.time / 1000000.0);
+        }
+    }
+
 private:
     std::vector<ReportStep> steps;
 };

955bac29e7c653fe9f09c22fac11bcb631066162
Kristian Skov Johansen <kris701kj@gmail.com>
2022-10-07 10:39:50 +0200

    Had to remove some tests i guess

--- Test/PlanValidators/PlanValidator.cpp
+++ Test/PlanValidators/PlanValidator.cpp
@@ -10,35 +10,6 @@ const string domainFile = "./TestFiles/gripper.pddl";
 const string problemFile = "./TestFiles/gripper-4.pddl";
 const string goodPlan = "./TestFiles/good_plan";
 const string badPlan = "./TestFiles/bad_plan";
-const filesystem::path validatorPath = filesystem::path("/root/downward-projects/VAL/validate");
-
-TEST_CASE(TAG + "ValidateGoodPlan") {
-    if (!FileHelper::DoesFileExist(validatorPath)) {
-        WARN("Cannot run PlanValidator test. VAL was not found! Skipping test...");
-    }
-    else {
-        Config config;
-        config.validatorPath = validatorPath;
-
-        PlanValidator validator;
-        PlanValidator::ValidatorResult res = validator.ValidatePlan(config, domainFile, problemFile, goodPlan);
-        REQUIRE(PlanValidator::ValidatorResult::PlanMatch == res);
-    }
-}
-
-TEST_CASE(TAG + "ValidateBadPlan") {
-    if (!FileHelper::DoesFileExist(validatorPath)) {
-        WARN("Cannot run PlanValidator test. VAL was not found! Skipping test...");
-    }
-    else {
-        Config config;
-        config.validatorPath = validatorPath;
-
-        PlanValidator validator;
-        PlanValidator::ValidatorResult res = validator.ValidatePlan(config, domainFile, problemFile, badPlan);
-        REQUIRE(PlanValidator::ValidatorResult::PlanNotMatch == res);
-    }
-}
 
 TEST_CASE(TAG + "MissingVAL") {
     Config config;

ef7b3e9ff079a0922a5000ab5258f320f67cccd4
Kristian Skov Johansen <kris701kj@gmail.com>
2022-10-10 10:21:50 +0200

    * Added options for pointer and instanciated versions of both the methods in the string helper * Also tested them

--- Test/Helpers/StringHelper.cpp
+++ Test/Helpers/StringHelper.cpp
@@ -7,28 +7,34 @@ const string TAG = "StringHelper ";
 
 #pragma region RemoveCharacter
 
-TEST_CASE(TAG + "RemoveCharacter_Single1") {
+TEST_CASE(TAG + "RemoveCharacter_None_Ptr") {
+    string line = "a b c d/(2";
+    StringHelper::RemoveCharacter(&line, 'q');
+    REQUIRE(line == line);
+}
+
+TEST_CASE(TAG + "RemoveCharacter_Single1_Ptr") {
     string line = "a b c d/(2";
     string expectedLine = " b c d/(2";
     StringHelper::RemoveCharacter(&line, 'a');
     REQUIRE(line == expectedLine);
 }
 
-TEST_CASE(TAG + "RemoveCharacter_Single2") {
+TEST_CASE(TAG + "RemoveCharacter_Single2_Ptr") {
     string line = "a b c d/(2";
     string expectedLine = "abcd/(2";
     StringHelper::RemoveCharacter(&line, ' ');
     REQUIRE(line == expectedLine);
 }
 
-TEST_CASE(TAG + "RemoveCharacter_Single3") {
+TEST_CASE(TAG + "RemoveCharacter_Single3_Ptr") {
     string line = "a b c d/(2";
     string expectedLine = "a b  d/(2";
     StringHelper::RemoveCharacter(&line, 'c');
     REQUIRE(line == expectedLine);
 }
 
-TEST_CASE(TAG + "RemoveCharacter_Multiple1") {
+TEST_CASE(TAG + "RemoveCharacter_Multiple1_Ptr") {
     string line = "a b c d/(2";
     string expectedLine = "   d/(2";
     StringHelper::RemoveCharacter(&line, 'a');
@@ -37,7 +43,7 @@ TEST_CASE(TAG + "RemoveCharacter_Multiple1") {
     REQUIRE(line == expectedLine);
 }
 
-TEST_CASE(TAG + "RemoveCharacter_Multiple2") {
+TEST_CASE(TAG + "RemoveCharacter_Multiple2_Ptr") {
     string line = "a b c d/(2";
     string expectedLine = "d/(2";
     StringHelper::RemoveCharacter(&line, 'a');
@@ -47,25 +53,104 @@ TEST_CASE(TAG + "RemoveCharacter_Multiple2") {
     REQUIRE(line == expectedLine);
 }
 
+TEST_CASE(TAG + "RemoveCharacter_None_Ins") {
+    string line = "a b c d/(2";
+    line = StringHelper::RemoveCharacter(line, 'q');
+    REQUIRE(line == line);
+}
+
+TEST_CASE(TAG + "RemoveCharacter_Single1_Ins") {
+    string line = "a b c d/(2";
+    string expectedLine = " b c d/(2";
+    line = StringHelper::RemoveCharacter(line, 'a');
+    REQUIRE(line == expectedLine);
+}
+
+TEST_CASE(TAG + "RemoveCharacter_Single2_Ins") {
+    string line = "a b c d/(2";
+    string expectedLine = "abcd/(2";
+    line = StringHelper::RemoveCharacter(line, ' ');
+    REQUIRE(line == expectedLine);
+}
+
+TEST_CASE(TAG + "RemoveCharacter_Single3_Ins") {
+    string line = "a b c d/(2";
+    string expectedLine = "a b  d/(2";
+    line = StringHelper::RemoveCharacter(line, 'c');
+    REQUIRE(line == expectedLine);
+}
+
+TEST_CASE(TAG + "RemoveCharacter_Multiple1_Ins") {
+    string line = "a b c d/(2";
+    string expectedLine = "   d/(2";
+    line = StringHelper::RemoveCharacter(line, 'a');
+    line = StringHelper::RemoveCharacter(line, 'b');
+    line = StringHelper::RemoveCharacter(line, 'c');
+    REQUIRE(line == expectedLine);
+}
+
+TEST_CASE(TAG + "RemoveCharacter_Multiple2_Ins") {
+    string line = "a b c d/(2";
+    string expectedLine = "d/(2";
+    line = StringHelper::RemoveCharacter(line, 'a');
+    line = StringHelper::RemoveCharacter(line, 'b');
+    line = StringHelper::RemoveCharacter(line, 'c');
+    line = StringHelper::RemoveCharacter(line, ' ');
+    REQUIRE(line == expectedLine);
+}
+
 #pragma endregion
 
-TEST_CASE(TAG + "Trim_Single1") {
+TEST_CASE(TAG + "Trim_None1_Ins") {
+    string line = "text text";
+    line = StringHelper::Trim(line);
+    REQUIRE(line == line);
+}
+
+TEST_CASE(TAG + "Trim_Single1_Ins") {
     string line = " text text ";
     string expectedLine = "text text";
     line = StringHelper::Trim(line);
     REQUIRE(line == expectedLine);
 }
 
-TEST_CASE(TAG + "Trim_Single2") {
+TEST_CASE(TAG + "Trim_Single2_Ins") {
     string line = "      text text ";
     string expectedLine = "text text";
     line = StringHelper::Trim(line);
     REQUIRE(line == expectedLine);
 }
 
-TEST_CASE(TAG + "Trim_Single3") {
+TEST_CASE(TAG + "Trim_Single3_Ins") {
     string line = "      text text                    ";
     string expectedLine = "text text";
     line = StringHelper::Trim(line);
+    REQUIRE(line == expectedLine);
+}
+
+TEST_CASE(TAG + "Trim_None1_Ptr") {
+    string line = "text text";
+    StringHelper::Trim(&line);
+    REQUIRE(line == line);
+}
+
+TEST_CASE(TAG + "Trim_Single1_Ptr") {
+    string line = " text text ";
+    string expectedLine = "text text";
+    StringHelper::Trim(&line);
+    REQUIRE(line == expectedLine);
+}
+
+TEST_CASE(TAG + "Trim_Single2_Ptr") {
+    string line = "      text text ";
+    string expectedLine = "text text";
+    StringHelper::Trim(&line);
+    REQUIRE(line == expectedLine);
+}
+
+TEST_CASE(TAG + "Trim_Single3_Ptr") {
+    string line = "      text text                    ";
+    string expectedLine = "text text";
+    StringHelper::Trim(&line);
     REQUIRE(line == expectedLine);
 }

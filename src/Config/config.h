d5ce0e27f617a248ae4784e4a2bb32a4d462f3fd
Kristian Skov Johansen <kris701kj@gmail.com>
2022-10-10 13:52:19 +0200

    Changed Flag to be string instead, easier to work with

--- src/Config/config.h
+++ src/Config/config.h
@@ -9,7 +9,7 @@ using namespace std;
 
 template <typename T>
 struct ConfigItem {
-    ConfigItem(char flag, string longFlag, string name, string description, T defaultContent) {
+    ConfigItem(string flag, string longFlag, string name, string description, T defaultContent) {
         Flag = flag;
         LongFlag = longFlag;
         Name = name;
@@ -19,25 +19,25 @@ struct ConfigItem {
 
     string Name;
     string Description;
-    char Flag;
+    string Flag;
     string LongFlag;
     T Content;
     T DefaultContent;
 };
 
 struct Options {
-    ConfigItem<string> Search = ConfigItem<string>('s', "search", "Fast Downward Search", "", "astar");
-    ConfigItem<string> Heuristic = ConfigItem<string>('e', "evaluator", "Fast Downward Heuristic", "", "blind");
+    ConfigItem<string> Search = ConfigItem<string>("s", "search", "Fast Downward Search", "", "astar");
+    ConfigItem<string> Heuristic = ConfigItem<string>("e", "evaluator", "Fast Downward Heuristic", "", "blind");
 };
 
 class Config {
 public:
-    ConfigItem<bool> DebugMode = ConfigItem<bool>('c', "debugmode", "Debug Mode", "Enable additional debugging info and checks", false);
-    ConfigItem<string> DownwardPath = ConfigItem<string>('f', "downwardpath", "Fast-Downward Path", "fast-downward.py filepath", "fast-downward.py");
+    ConfigItem<bool> DebugMode = ConfigItem<bool>("c", "debugmode", "Debug Mode", "Enable additional debugging info and checks", false);
+    ConfigItem<string> DownwardPath = ConfigItem<string>("f", "downwardpath", "Fast-Downward Path", "fast-downward.py filepath", "fast-downward.py");
     Options DownwardOptions;
-    ConfigItem<string> ValidatorPath = ConfigItem<string>('v', "validatorpath", "VAL validator Path", "validator filepath", "validate");
-    ConfigItem<string> DomainFile = ConfigItem<string>('d', "domain", "Domain File", "Path to domain file", "gripper_domain.pddl");
-    ConfigItem<string> ProblemFile = ConfigItem<string>('p', "problem", "Problem File", "Path to problem file", "gripper_problem.pddl");
+    ConfigItem<string> ValidatorPath = ConfigItem<string>("v", "validatorpath", "VAL validator Path", "validator filepath", "validate");
+    ConfigItem<string> DomainFile = ConfigItem<string>("d", "domain", "Domain File", "Path to domain file", "gripper_domain.pddl");
+    ConfigItem<string> ProblemFile = ConfigItem<string>("p", "problem", "Problem File", "Path to problem file", "gripper_problem.pddl");
 
     int ParseArgs(Config* config, int argc, char** argv);
 

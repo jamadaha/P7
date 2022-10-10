58c4dde0dc728204b6ebfc5500e4bc28cb1b8240
Christoffer Lind Andersen <chri692u@gmail.com>
2022-10-10 09:40:47 +0200

    start of Test PDDLTypes

---
+++ Test/PDDLTypes/PDDLTypes.cpp
@@ -0,0 +1,42 @@
+#include <catch2/catch_test_macros.hpp>
+#include <string>
+
+#include "../../src/PDDLTypes/PDDLDomain.hpp"
+#include "../../src/PDDLParser/pddldriver.hh"
+
+using namespace std;
+
+
+const string TAG = "PDDLTypes ";
+
+const string domainFile = "./TestFiles/domain.pddl";
+const string exDomainName = "blocksworld";
+const vector<string> exDomainReq = {":strips", ":equality"};
+
+bool cmpList(vector<string> as, vector<string> bs){
+    bool acc = true;
+    for(string a : as)
+        for (string b : bs)
+            acc = acc && (a==b);
+    return acc;
+}
+
+TEST_CASE(TAG + "PDDLDomain names + requirements") {
+    PDDLDriver driver = PDDLDriver();
+    driver.parse(domainFile);
+    PDDLDomain domain = PDDLDomain(driver.domain);
+
+    REQUIRE(domain.name == exDomainName);
+    REQUIRE(cmpList(domain.requirements, exDomainReq));
+}
+
+TEST_CASE(TAG + "PDDLDomain actions"){
+    PDDLDriver driver;
+    driver.parse(domainFile);
+    PDDLDomain domain = PDDLDomain(driver.domain);
+    REQUIRE(true);
+}
+
+TEST_CASE(TAG + "update?"){
+    REQUIRE(true);
+}

10e4fe2b333be03e14b5234583e8a87e28af254e
Christoffer Lind Andersen <chri692u@gmail.com>
2022-10-11 08:25:40 +0200

    Update P7.cpp

--- src/P7.cpp
+++ src/P7.cpp
@@ -1,56 +1,18 @@
 ﻿#include <iostream>
-#include <memory>
-#include <unordered_set>
-#include "PDDLParser/pddldriver.hh"
-#include "SASParser/SASParser.h"
-#include "PDDLCodeGenerator/PDDLCodeGenerator.h"
 #include "Config/config.h"
 #include "SASCodeGenerator/SASCodeGenerator.h"
-#include "FileVerifier/FileVerifier.h"
 #include "CommonInterface/CommonInterface.h"
 #include "Reformulators/SameOutputReformulator.h"
-#include "Heuristics/BaseHeuristics.hh"
-#include "Heuristics/RandomHeuristic.hh"
 
 #include "Report.hh"
 
 using namespace std;
 
 int main(int argc, char** argv)
-{   
-    PDDLDriver driver;
-    driver.parse("./TestFiles/gripper.pddl");
-    PDDLDomain *domain = new PDDLDomain(driver.domain);
-
-    auto acts = domain->actions;
-    unordered_set<string> args;
-	vector<string> argString;
-
-    for (auto a : acts)
-        for (auto arg : a.parameters)
-            args.insert(arg.name);
-
-	for (string a : args)
-		argString.push_back(a);
-
-	for (auto a : argString)
-		cout << a << endl;
-	/*
-	PDDLDriver driver;
-    driver.parse("./TestFiles/gripper.pddl");
-    PDDLDomain *domain = new PDDLDomain(driver.domain);
-
-    auto preds = domain->predicates;
-    vector<string> predStrings;
-
-    for (auto p : preds)
-        predStrings.push_back(p.name);
-	for (string s : predStrings)
-		cout << s << endl;
-	/*
+{
 	Config config;
 	// Do first as it quits on help
-	if (config.parseArgs(&config, argc, argv))
+	if (config.ParseArgs(&config, argc, argv))
 		return 0;
 
 	Report report = Report();
@@ -77,5 +39,4 @@ int main(int argc, char** argv)
 	if (result == CommonInterface::RunResult::RanWithoutErrors)
 		report.Print();
 	return 0;
-	*/
 }

da09daae9dedd11d94ac9ce89b43dcfb8e7c14f8
Christoffer Lind Andersen <chri692u@gmail.com>
2022-10-12 13:38:17 +0200

    Fix please christo haha

--- Test/DownwardRunner/DownwardRunner.cpp
+++ Test/DownwardRunner/DownwardRunner.cpp
@@ -10,7 +10,21 @@ const string TAG = "DownwardRunner ";
 const string domain = "./TestFiles/gripper-4.pddl";
 const string problem = "./TestFiles/gripper.pddl";
 
-TEST_CASE(TAG + "Run domain"){
+TEST_CASE(TAG + "Run a legal domain"){
+    DownwardRunner downward = DownwardRunner();
+    Config config;
+    auto x = downward.RunDownward(config, domain, problem);
+    REQUIRE(x == DownwardRunner::DownwardRunnerResult::FoundPlan);
+}
+
+TEST_CASE(TAG + "Run impossible domain"){
+    DownwardRunner downward = DownwardRunner();
+    Config config;
+    auto x = downward.RunDownward(config, domain, problem);
+    REQUIRE(x == DownwardRunner::DownwardRunnerResult::DidNotFindPlan);
+}
+
+TEST_CASE(TAG + "Run illegal domain"){
     DownwardRunner downward = DownwardRunner();
     Config config;
     auto x = downward.RunDownward(config, domain, problem);

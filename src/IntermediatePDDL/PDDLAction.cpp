55f8d3256d5fba5ea688efa08ae6bb4e6ce05e6f
jamadaha <53914190+jamadaha@users.noreply.github.com>
2022-10-22 17:58:49 +0200

    Fixes for copy constructor of PDDLAction

--- src/IntermediatePDDL/PDDLAction.cpp
+++ src/IntermediatePDDL/PDDLAction.cpp
@@ -2,6 +2,8 @@
 
 std::unordered_map<unsigned int, const std::unordered_set<const PDDLLiteral*>> PDDLAction::GenerateApplicableLiterals(bool unary) {
     std::unordered_map<unsigned int, const std::unordered_set<const PDDLLiteral*>> set;
+    if (parameters.size() == 0 || preconditions.size() == 0)
+        return set;
     for (int i = 0; i < parameters.size(); i++) {
         std::unordered_set<const PDDLLiteral*> tempSet;
         for (int literalIndex = 0; literalIndex < preconditions.size(); literalIndex++) {

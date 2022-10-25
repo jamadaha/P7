7b0325655d62d8c3f86818ad88cabf02f623443b
jamadaha <53914190+jamadaha@users.noreply.github.com>
2022-10-25 04:47:53 +0200

    Adds applicable predicates

--- src/IntermediatePDDL/PDDLAction.hh
+++ src/IntermediatePDDL/PDDLAction.hh
@@ -15,16 +15,21 @@ struct PDDLAction {
     const std::vector<PDDLLiteral> preconditions;
     const std::vector<PDDLLiteral> effects;
     // For each parameter, what unary preconditions mention it
-    const std::unordered_map<unsigned int, const std::unordered_set<const PDDLLiteral*>> applicableUnaryLiterals;
+    const std::vector<std::unordered_set<const PDDLLiteral*>> applicableUnaryLiterals;
+    // For each parameter, what unary predicates are mentioned in applicable preconditions
+    const std::vector<std::unordered_set<unsigned int>> applicableUnaryPredicates;
     // For each parameter, what non unary preconditions mention it
-    const std::unordered_map<unsigned int, const std::unordered_set<const PDDLLiteral*>> applicableMultiLiterals;
+    const std::vector<std::unordered_set<const PDDLLiteral*>> applicableMultiLiterals;
     PDDLAction() : name("Not Set") {};
 
     PDDLAction(std::string name, std::vector<std::string> parameters, std::vector<PDDLLiteral> preconditions, std::vector<PDDLLiteral> effects) : 
         name(name), parameters(parameters), preconditions(preconditions), effects(effects), applicableUnaryLiterals(GenerateApplicableLiterals(true)), applicableMultiLiterals(GenerateApplicableLiterals(false)) {};
 
     PDDLAction(const PDDLAction &a) : 
-        name(a.name), parameters(a.parameters), preconditions(a.preconditions), effects(a.effects), applicableUnaryLiterals(GenerateApplicableLiterals(true)), applicableMultiLiterals(GenerateApplicableLiterals(false)) {};
+        name(a.name), parameters(a.parameters), preconditions(a.preconditions), effects(a.effects), 
+        applicableUnaryLiterals(GenerateApplicableLiterals(true)), 
+        applicableMultiLiterals(GenerateApplicableLiterals(false)),
+        applicableUnaryPredicates(GenerateApplicablePredicates(true)) {};
 
     /// @return Returns true if name, parameters and preconditions are the same, ignores parameter names
     friend bool operator==(const PDDLAction& lhs, const PDDLAction& rhs) {
@@ -40,7 +45,8 @@ struct PDDLAction {
     }
 
 private:
-    std::unordered_map<unsigned int, const std::unordered_set<const PDDLLiteral*>> GenerateApplicableLiterals(bool unary);
+    std::vector<std::unordered_set<const PDDLLiteral*>> GenerateApplicableLiterals(bool unary) const;
+    std::vector<std::unordered_set<unsigned int>> GenerateApplicablePredicates(bool unary) const;
 };
 
 #endif

6feb56e0cdf0997d5b1fe56ca86e8b97c1d3c6db
Dovy <djasulaitis05@gmail.com>
2022-11-01 09:29:16 +0100

    make tests fail

--- Test/MacroGenerator/MacroGenerator.cpp
+++ Test/MacroGenerator/MacroGenerator.cpp
@@ -1,25 +1,25 @@
 #include <catch2/catch_test_macros.hpp>
 
-#include "../src/MacroGenerator/MacroGenerator.hpp"
+#include "../../src/MacroGenerator/MacroGenerator.hh"
 
 const std::string TAG = "Macro ";
 
 TEST_CASE(TAG + "two different actions") {
-    
+    REQUIRE(1==0);
 }
 
 TEST_CASE(TAG + "overlapping preconditions") {
-
+    REQUIRE(1==0);
 }
 
 TEST_CASE(TAG + "precon - positive eff") {
-
+    REQUIRE(1==0);
 }
 
 TEST_CASE(TAG + "positive - negative eff") {
-
+    REQUIRE(1==0);
 }
 
 TEST_CASE(TAG + "negative - positive eff") {
-    
+    REQUIRE(1==0);
 }

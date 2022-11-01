0e74eaec3c57360800fff696790ef7ec15c12bc4
Dovy <djasulaitis05@gmail.com>
2022-11-01 09:21:35 +0100

    make macro test

---
+++ Test/MacroGenerator/MacroGenerator.cpp
@@ -0,0 +1,23 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "../src/MacroGenerator/MacroGenerator.hpp"
+
+const std::string TAG = "Macro ";
+
+TEST_CASE(TAG + "two different actions") {
+    
+}
+
+TEST_CASE(TAG + "overlapping preconditions") {
+
+}
+
+TEST_CASE(TAG + "precon - positive eff") {
+    
+}
+
+TEST_CASE(TAG + "positive - negative eff") {
+
+}
+
+TEST_CASE(TAG + "negative - positive eff")

6cf224612264299651b21255e2737b7fc5f9bc52
Dovy <djasulaitis05@gmail.com>
2022-11-01 11:58:07 +0100

    implement more test

--- Test/MacroGenerator/MacroGenerator.cpp
+++ Test/MacroGenerator/MacroGenerator.cpp
@@ -9,14 +9,47 @@ const std::string TAG = "Macro ";
 TEST_CASE(TAG + "two same simple actions") {
     std::vector<PDDLActionInstance*> actions;
     // set up simple action with 1 precondition and 1 effect
-    PDDLAction act = PDDLAction(
+    PDDLAction _act = PDDLAction(
         "testaction",
         std::vector<std::string> {"?x", "?y"},
         std::vector<PDDLLiteral> {PDDLLiteral(1, std::vector<unsigned int> {0}, true)},
         std::vector<PDDLLiteral> {PDDLLiteral(2, std::vector<unsigned int> {1}, true)}
     );
-    PDDLActionInstance act1 = PDDLActionInstance(&act, std::vector<unsigned int> {0, 1});
-    PDDLActionInstance act2 = PDDLActionInstance(&act, std::vector<unsigned int> {1, 2});
+    PDDLActionInstance act1 = PDDLActionInstance(&_act, std::vector<unsigned int> {0, 1});
+    PDDLActionInstance act2 = PDDLActionInstance(&_act, std::vector<unsigned int> {0, 1});
+    actions.push_back(&act1);
+    actions.push_back(&act2);
+    // generate macro
+    MacroGenerator macroGenerator = MacroGenerator();
+    Macro m = macroGenerator.GenerateMacro(&actions);
+    // expected stuff
+    std::unordered_map<GroundedLiteral, bool> expectedPrecons ({
+        {GroundedLiteral(1, std::vector<unsigned int> {0}), true}
+    });
+    std::unordered_map<GroundedLiteral, bool> expectedEffects ({
+        {GroundedLiteral(2, std::vector<unsigned int> {1}), true}
+    });
+    REQUIRE(m.groundedAction.parameters == std::unordered_set<unsigned int> {0, 1});
+    REQUIRE(m.groundedAction.preconditions == expectedPrecons);
+    REQUIRE(m.groundedAction.effects == expectedEffects);
+}
+
+TEST_CASE(TAG + "two different simple actions") {
+    std::vector<PDDLActionInstance*> actions;
+    PDDLAction _act1 = PDDLAction(
+        "testaction1",
+        std::vector<std::string> {"?x", "?y"},
+        std::vector<PDDLLiteral> {PDDLLiteral(1, std::vector<unsigned int> {0}, true)},
+        std::vector<PDDLLiteral> {PDDLLiteral(2, std::vector<unsigned int> {1}, true)}
+    );
+    PDDLAction _act2 = PDDLAction(
+        "testaction2",
+        std::vector<std::string> {"?x", "?y"},
+        std::vector<PDDLLiteral> {PDDLLiteral(3, std::vector<unsigned int> {0}, false)},
+        std::vector<PDDLLiteral> {PDDLLiteral(4, std::vector<unsigned int> {1}, true)}
+    );
+    PDDLActionInstance act1 = PDDLActionInstance(&_act1, std::vector<unsigned int> {0, 1});
+    PDDLActionInstance act2 = PDDLActionInstance(&_act2, std::vector<unsigned int> {1, 3});
     actions.push_back(&act1);
     actions.push_back(&act2);
     // generate macro
@@ -25,23 +58,15 @@ TEST_CASE(TAG + "two same simple actions") {
     // expected stuff
     std::unordered_map<GroundedLiteral, bool> expectedPrecons ({
         {GroundedLiteral(1, std::vector<unsigned int> {0}), true},
-        {GroundedLiteral(1, std::vector<unsigned int> {1}), true}
+        {GroundedLiteral(3, std::vector<unsigned int> {1}), false}
     });
     std::unordered_map<GroundedLiteral, bool> expectedEffects ({
         {GroundedLiteral(2, std::vector<unsigned int> {1}), true},
-        {GroundedLiteral(2, std::vector<unsigned int> {2}), true}
+        {GroundedLiteral(4, std::vector<unsigned int> {3}), true}
     });
-    REQUIRE(m.groundedAction.parameters == std::unordered_set<unsigned int> {0, 1, 2});
+    REQUIRE(m.groundedAction.parameters == std::unordered_set<unsigned int> {0, 1, 3});
     REQUIRE(m.groundedAction.preconditions == expectedPrecons);
     REQUIRE(m.groundedAction.effects == expectedEffects);
-}
-
-TEST_CASE(TAG + "two different simple actions") {
-    REQUIRE(1==0);
-}
-
-TEST_CASE(TAG + "overlapping preconditions") {
-    REQUIRE(1==0);
 }
 
 TEST_CASE(TAG + "precon - positive eff") {

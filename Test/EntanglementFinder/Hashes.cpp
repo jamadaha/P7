d583b7f20d2e3d7e4efbbfe4d85412309ffb5520
Kristian Skov <kris701kj@gmail.com>
2022-11-01 10:45:09 +0100

    * Changed a hash function (its a bit slower, but its pretty important that this specific one is good) * Added futher tests

--- Test/EntanglementFinder/Hashes.cpp
+++ Test/EntanglementFinder/Hashes.cpp
@@ -1,11 +1,19 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include <vector>
+#include <string>
 
 #include "../../src/EntanglementFinder/EntanglementFinder.hh"
 #include "../../src/Walker/Walker.hpp"
 
-int collisionDepth = 100;
+int collisionDepth = 5000;
+int currentCase = 1;
+int totalCases = 17;
+bool debugPrint = true;
+void FinishedCase() {
+    if (debugPrint)
+        std::cout << "EntanglementFinder-Hashes - Finished " + std::to_string(currentCase++) + " out of " + std::to_string(totalCases) << std::endl;
+}
 
 using namespace std;
 
@@ -19,21 +27,59 @@ TEST_CASE(TAG + "Can_Hash_PDDLActionInstance") {
 
     auto hashValue = constantInstance.GetHash();
 
-    REQUIRE(hashValue == 6505548967629018342);
+    REQUIRE(hashValue == 6505549070604997458);
+    FinishedCase();
 }
 
 TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Objects_NoCollision_1") {
     unordered_set<size_t> hashes;
-    PDDLAction constantAction;
+    vector<string> parameters;
+    parameters.reserve(1);
+    vector<PDDLLiteral> preconditions;
+    vector<PDDLLiteral> effects;
+    PDDLAction constantAction = PDDLAction("Name", parameters, preconditions, effects);
     for (int i = 0; i < collisionDepth; i++) {
-        PDDLActionInstance constantInstance(&constantAction, { (unsigned int)i, (unsigned int)i + 1, (unsigned int)i + 2 });
+        PDDLActionInstance constantInstance(&constantAction, { (unsigned int)i });
         auto hashValue = constantInstance.GetHash();
         REQUIRE(!hashes.contains(hashValue));
         hashes.emplace(hashValue);
     }
+    FinishedCase();
 }
 
 TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Objects_NoCollision_2") {
+    unordered_set<size_t> hashes;
+    vector<string> parameters;
+    parameters.reserve(2);
+    vector<PDDLLiteral> preconditions;
+    vector<PDDLLiteral> effects;
+    PDDLAction constantAction = PDDLAction("Name", parameters, preconditions, effects);
+    for (int i = 0; i < collisionDepth; i++) {
+        PDDLActionInstance constantInstance(&constantAction, { (unsigned int)i, (unsigned int)i });
+        auto hashValue = constantInstance.GetHash();
+        REQUIRE(!hashes.contains(hashValue));
+        hashes.emplace(hashValue);
+    }
+    FinishedCase();
+}
+
+TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Objects_NoCollision_3") {
+    unordered_set<size_t> hashes;
+    vector<string> parameters;
+    parameters.reserve(3);
+    vector<PDDLLiteral> preconditions;
+    vector<PDDLLiteral> effects;
+    PDDLAction constantAction = PDDLAction("Name", parameters, preconditions, effects);
+    for (int i = 0; i < collisionDepth; i++) {
+        PDDLActionInstance constantInstance(&constantAction, { (unsigned int)i, (unsigned int)i, (unsigned int)i });
+        auto hashValue = constantInstance.GetHash();
+        REQUIRE(!hashes.contains(hashValue));
+        hashes.emplace(hashValue);
+    }
+    FinishedCase();
+}
+
+TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Objects_NoCollision_4") {
     unordered_set<size_t> hashes;
     PDDLAction constantAction;
     for (int i = 0; i < collisionDepth; i++) {
@@ -42,9 +88,10 @@ TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Objects_NoCollision_2") {
         REQUIRE(!hashes.contains(hashValue));
         hashes.emplace(hashValue);
     }
+    FinishedCase();
 }
 
-TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Objects_NoCollision_3") {
+TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Objects_NoCollision_5") {
     unordered_set<size_t> hashes;
     PDDLAction constantAction;
     for (int i = 0; i < collisionDepth; i++) {
@@ -57,6 +104,7 @@ TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Objects_NoCollision_3") {
         REQUIRE(!hashes.contains(hashValue));
         hashes.emplace(hashValue);
     }
+    FinishedCase();
 }
 
 TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Objects_Collision_1") {
@@ -70,6 +118,7 @@ TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Objects_Collision_1") {
     auto hashValue2 = constantInstance2.GetHash();
 
     REQUIRE(hashValue1 == hashValue2);
+    FinishedCase();
 }
 
 TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Objects_Collision_2") {
@@ -89,6 +138,7 @@ TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Objects_Collision_2") {
     auto hashValue2 = constantInstance2.GetHash();
 
     REQUIRE(hashValue1 == hashValue2);
+    FinishedCase();
 }
 
 TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Action_NoCollision_1") {
@@ -100,6 +150,7 @@ TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Action_NoCollision_1") {
         REQUIRE(!hashes.contains(hashValue));
         hashes.emplace(hashValue);
     }
+    FinishedCase();
 }
 
 TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Action_NoCollision_2") {
@@ -118,6 +169,7 @@ TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Action_NoCollision_2") {
         REQUIRE(!hashes.contains(hashValue));
         hashes.emplace(hashValue);
     }
+    FinishedCase();
 }
 
 TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Action_Collision_1") {
@@ -134,8 +186,107 @@ TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Action_Collision_1") {
     PDDLActionInstance constantInstance2(&constantAction2, { 1 });
 
     REQUIRE(constantInstance1.GetHash() == constantInstance2.GetHash());
+    FinishedCase();
 }
 
 #pragma endregion
 
+#pragma region vector<PDDLActionInstance>
 
+TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Vector") {
+    vector<PDDLActionInstance*> set;
+    for (int i = 0; i < 10; i++) {
+        PDDLAction constantAction;
+        set.push_back(new PDDLActionInstance(&constantAction, { 1, 2, (unsigned int)i }));
+    }
+
+    auto hashValue = hash<vector<PDDLActionInstance*>>{}(set);
+
+    REQUIRE(hashValue == 4344514121280611648);
+    FinishedCase();
+}
+
+TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Vector_NoCollision_1") {
+    unordered_set<size_t> hashes;
+    for (int i = 0; i < collisionDepth; i++) {
+        vector<PDDLActionInstance*> set;
+        for (int j = i; j < i + 10; j++) {
+            PDDLAction constantAction = PDDLAction("Name-" + to_string(j));
+            set.push_back(new PDDLActionInstance(&constantAction, { 1, 2, (unsigned int)j }));
+        }
+
+        auto hashValue = hash<vector<PDDLActionInstance*>>{}(set);
+        REQUIRE(!hashes.contains(hashValue));
+        hashes.emplace(hashValue);
+    }
+    FinishedCase();
+}
+
+TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Vector_NoCollision_2") {
+    unordered_set<size_t> hashes;
+    for (int i = 0; i < collisionDepth; i++) {
+        vector<PDDLActionInstance*> set;
+        for (int j = 0; j < i; j++) {
+            PDDLAction constantAction = PDDLAction("Name-" + to_string(j));
+            set.push_back(new PDDLActionInstance(&constantAction, { 1, 2, (unsigned int)j }));
+        }
+
+        auto hashValue = hash<vector<PDDLActionInstance*>>{}(set);
+        REQUIRE(!hashes.contains(hashValue));
+        hashes.emplace(hashValue);
+    }
+    FinishedCase();
+}
+
+TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Vector_Collision_1") {
+    
+    size_t expectedHash = 2149843915544583187;
+
+    for (int i = 0; i < collisionDepth; i++) {
+        vector<PDDLActionInstance*> set;
+        for (int j = 0; j < 2; j++) {
+            PDDLAction constantAction = PDDLAction("Name-" + to_string(j));
+            set.push_back(new PDDLActionInstance(&constantAction, { 1, 2, (unsigned int)j }));
+        }
+
+        auto hashValue = hash<vector<PDDLActionInstance*>>{}(set);
+        REQUIRE(hashValue == expectedHash);
+    }
+    FinishedCase();
+}
+
+TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Vector_Collision_2") {
+
+    size_t expectedHash = 1555253033455459816;
+
+    for (int i = 0; i < collisionDepth; i++) {
+        vector<PDDLActionInstance*> set;
+        for (int j = 0; j < 10; j++) {
+            PDDLAction constantAction = PDDLAction("Name-" + to_string(j));
+            set.push_back(new PDDLActionInstance(&constantAction, { 1, 2, (unsigned int)j }));
+        }
+
+        auto hashValue = hash<vector<PDDLActionInstance*>>{}(set);
+        REQUIRE(hashValue == expectedHash);
+    }
+    FinishedCase();
+}
+
+TEST_CASE(TAG + "Can_Hash_PDDLActionInstance_Vector_Collision_3") {
+
+    size_t expectedHash = 16476306162301439505;
+
+    for (int i = 0; i < collisionDepth; i++) {
+        vector<PDDLActionInstance*> set;
+        for (int j = 0; j < 100; j++) {
+            PDDLAction constantAction = PDDLAction("Name-" + to_string(j));
+            set.push_back(new PDDLActionInstance(&constantAction, { 1, 2, (unsigned int)j }));
+        }
+
+        auto hashValue = hash<vector<PDDLActionInstance*>>{}(set);
+        REQUIRE(hashValue == expectedHash);
+    }
+    FinishedCase();
+}
+
+#pragma endregion

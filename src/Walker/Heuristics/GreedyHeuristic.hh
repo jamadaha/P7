5fe56352d7c1ff3e16c7d98b068cb965b3bfca64
Christoffer Lind Andersen <chri692u@gmail.com>
2022-11-02 12:06:03 +0100

    Update GreedyHeuristic.hh

--- src/Walker/Heuristics/GreedyHeuristic.hh
+++ src/Walker/Heuristics/GreedyHeuristic.hh
@@ -9,11 +9,7 @@ public:
 	GreedyHeuristic(const PDDLDomain *domain, const PDDLProblem *problem) : BaseHeuristic(domain, problem){};
 
 	PDDLActionInstance* NextChoice(PDDLState * state, std::vector<PDDLActionInstance> *choices) const override {
-		return ;
-	}
-
-	int Eval(const PDDLState *state) const override {
-        int value = 0
+	    int value = 0
         std::set<std::string> candidates;
         std::pair<int, std::string> solution;
         std::unordered_map<int, std::string> solutions;
@@ -28,22 +24,54 @@ public:
                 for (auto i : candidates){
                     if (/*not in the solution vector*/){
                         /*Calculatings greed for each index*/
-                        solutions.enplace(Greed(i), /*id*/);
+                        solutions.enplace(this.Eval(i, solutions), /*id*/);
                     }
                 }
                 /*Add highest pair to solution*/
             }
-            if(solutionValue > value){
-                solution = solutionValue;
-                value = solutionValue
-            }
+            /*Sort solutions*/
+            /*solution = highest solution*/
         }
-        return value;
-	};
+        /*return new state at best value (solution)*/
+	}
 
-    int Greed(const PDDLState *state) const {
-        GoalCountHeuristic gch = GoalCountHeuristic(domain, problem);
-    }
+	int Eval(const PDDLState *state, /*std::unordered_map<int, std::string> solutions*/) const override {
+	};
 };
 
 #endif
+
+
+/* Christoffer fik maksimal head explosion af C++, så her er pseudokoden
+
+procedure Greedy-Search : State x Actions -> State
+    let value = 0
+    let candidates be a list
+    let solution be a (Int, String) tuple
+    let solutions be a (Int, String) map
+
+    for each object o in the State problem domain do:
+        candidates.insert(o)
+        do
+            if candidates = empty
+            then break 
+            else
+            for each string id in candidates do:
+                if the candidate is not already in the solution vector then
+                    let greed = call Greed-Score(candidate with id, solutions)
+                    solutions.push( (greed, id) )
+            sort solutions
+            let solution = highest in solutions
+        while(true)
+
+        return state with highest greed value
+
+sub-procedure Greedy-Score : State x States -> Int
+        sum = 0, greed = 0
+        let gch be the goal count heuristic
+
+        For each (i, s) in States do
+            sum += gch(s)
+        
+        return gch(State) / sum - gch(State)
+*/

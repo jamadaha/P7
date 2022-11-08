242cc55ce201261d7ad55aa7b68d849be7268fc8
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2022-11-08 08:33:49 +0100

    Moves files

---
+++ src/Reformulators/Walkers/BaseWalkerReformulator.hh
@@ -0,0 +1,13 @@
+#ifndef BASE_WALKER_REFORMULATOR
+#define BASE_WALKER_REFORMULATOR
+
+#include "../BaseReformulator.hh"
+
+class BaseWalkerReformulator : BaseReformulator {
+public:
+
+protected:
+
+};
+
+#endif

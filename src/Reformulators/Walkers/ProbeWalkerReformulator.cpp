71daf3c47452345e570d804f81fcf1290bf983ce
Kristian Skov <kris701kj@gmail.com>
2022-11-14 12:19:24 +0100

    Merge fixes

--- src/Reformulators/Walkers/ProbeWalkerReformulator.cpp
+++ src/Reformulators/Walkers/ProbeWalkerReformulator.cpp
@@ -2,7 +2,7 @@
 
 std::vector<Path> ProbeWalkerReformulator::PerformWalk(PDDLInstance* instance, bool debugMode) {
     auto walkerHeuistic = Configs->GetItem<std::string>("walkerHeuristic");
-    auto walker = WalkerBuilder::BuildWalker("walkerProbe", TimeLimit, walkerHeuistic, instance);
+    auto walker = WalkerBuilder::BuildWalker("walkerProbe", TimeLimit * (1 - Configs->GetItem<double>("reformulationTimeFraction")), walkerHeuistic, instance);
     if (debugMode)
         SetupWalkerDebugInfo(walker);
     return walker->Walk();

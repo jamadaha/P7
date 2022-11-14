5e6ec7b86c4dd6eba8763ce84a9e8e142a2d3dbb
Kristian Skov <kris701kj@gmail.com>
2022-11-14 21:10:53 +0100

    Actually got progress value to work

--- Lab/LabSuite.py
+++ Lab/LabSuite.py
@@ -1,8 +1,8 @@
-from lab import experiment
 from lab.experiment import Experiment
 from lab.environments import LocalEnvironment
 from downward import suites
 import shutil
+import os
 from os import path
 from Lab.Reports import Reports
 from Lab.Benchmarks import get_suite
@@ -55,9 +55,10 @@ class LabSuite():
 
             experiment = self._SetupExperiment(experimentSettings, labSettings.Threads, settingsFile)
 
-            experiment.set_property("p7_settings_file", settingsFile);
+            experiment.set_property("p7_settings_file", settingsFile);  
             self._SetupExperimentCleanupSteps(experiment, settingsFile)
             experiment.add_step("build", experiment.build)
+            experiment.add_step("addextralogging", self.AddLoggingToBuid)
             experiment.add_step("start", experiment.start_runs)
             experiment.add_fetcher(name="fetch")
 
@@ -71,6 +72,32 @@ class LabSuite():
 
             experiments.append(experiment)
 
+    def AddLoggingToBuid(self):
+        fileName = self._reportfolder + "/run"
+        lines = []
+        with open(fileName, 'r') as file:
+            lines = file.readlines()
+        
+        changedLines = []
+        for line in lines:
+            if "def process_task(task_id):" in line:
+                changedLines.append("def process_task(task_id, counter, counterMax):\n")
+            elif "result = pool.map_async(process_task, range(1, num_tasks + 1))" in line:
+                changedLines.append("    counters = range(1, num_tasks + 1)\n")
+                changedLines.append("    counterMaxs = [num_tasks] * num_tasks\n")
+                changedLines.append("    result = pool.starmap_async(process_task, zip(SHUFFLED_TASK_IDS, counters, counterMaxs))\n")
+            elif "logging.info(f\"Starting run {run_id} (TASK_ID {task_id}) in {run_dir}\")" in line:
+                changedLines.append("            logging.info(\"(\" + str(counter) + \"/\" + str(counterMax) + f\") Starting run {run_id} (TASK_ID {task_id}) in {run_dir}\")\n")
+            else:
+                changedLines.append(line)
+            if "def main():" in line:
+                changedLines.append("    counter = 0\n")
+
+        os.remove(fileName)
+
+        with open(fileName, 'w') as file:   
+            file.writelines(changedLines)
+
     def CombineReports(self):
         CSVGenerator.AddCSVReport(ATTRIBUTES)
 
@@ -82,16 +109,15 @@ class LabSuite():
                 self._SetupTasks(experimentSettings.SettingsContent, task, reformulator, experiment, settingsFile)
         return experiment
 
-    def _SetupTasks(self, content, task, reformulator, experiment, settingsFile):
+    def _SetupTasks(self, content, task, reformulator, experiment : Experiment, settingsFile):
         content += "\nPATH:domain=" + task.domain_file + "\n"
         content += "PATH:problem=" + task.problem_file + "\n"
 
         content += "LIST<STRING>:reformulator=" + reformulator + "\n"
-
+        
         run = experiment.add_run()
         run.add_new_file("config","TempSettings.ini",content)
         run.add_command("planner", [self._projectfile,"{config}"])
-
         run.set_property("id",[reformulator, task.domain, task.problem])
         run.set_property("domain", task.domain)
         run.set_property("problem", task.problem)

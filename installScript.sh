5c8419e12db20803b7e2a7f4b6065f5c6559ad26
Kristian Skov <kris701kj@gmail.com>
2022-11-16 17:00:11 +0100

    Fixed paths in the install script

--- installScript.sh
+++ installScript.sh
@@ -9,10 +9,10 @@ sudo apt update;
 # Install Catch2
 git clone https://github.com/catchorg/Catch2.git Catch2 && cd Catch2 && cmake -Bbuild -H. -DBUILD_TESTING=OFF && sudo cmake --build build/ --target install && cd ..;
 # Install Fast Downward
-sudo apt install cmake g++ git make python3 && git clone https://github.com/aibasel/downward.git && cd downward && ./build.py;
+sudo apt install cmake g++ git make python3 && git clone https://github.com/aibasel/downward.git && cd downward && ./build.py && cd ..;
 # Install VAL
-sudo apt install g++ make flex bison && git clone https://github.com/KCL-Planning/VAL.git && cd VAL && git checkout a5565396007eee73ac36527fbf904142b3077c74 && make clean && sed -i 's/-Werror //g' Makefile && make;
+sudo apt install g++ make flex bison && git clone https://github.com/KCL-Planning/VAL.git && cd VAL && git checkout a5565396007eee73ac36527fbf904142b3077c74 && make clean && sed -i 's/-Werror //g' Makefile && make && cd ..;
 # Install Lab
 sudo apt install python3-pip && pip install lab
 # Download data repo
-cd .. && cd.. && git clone https://github.com/jamadaha/P7_Data
+git clone https://github.com/jamadaha/P7_Data

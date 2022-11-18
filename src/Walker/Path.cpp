3b764bdfda812f98397e3d83e0b09839e299e443
Kristian Skov <kris701kj@gmail.com>
2022-11-18 10:05:24 +0100

    Added ToString for path

--- src/Walker/Path.cpp
+++ src/Walker/Path.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 string Path::ToString() {
-	return "abc";
+	string retStr = "";
+	for (auto step : steps) {
+		retStr += "(" + step.action->name + ", [";
+		for (auto object : step.objects)
+			retStr += to_string(object) + ", ";
+		retStr = retStr.substr(0, retStr.length() - 2);
+		retStr += "]) => ";
+	}
+	retStr = retStr.substr(0, retStr.length() - 4);
+
+	return retStr;
 }

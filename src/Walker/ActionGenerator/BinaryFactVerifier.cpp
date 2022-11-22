4e3fbc6332287f4b8e4de7649f4fd7fb38baa608
jamadaha <53914190+jamadaha@users.noreply.github.com>
2022-11-22 06:58:54 +0100

    Further refactor

--- src/Walker/ActionGenerator/BinaryFactVerifier.cpp
+++ src/Walker/ActionGenerator/BinaryFactVerifier.cpp
@@ -11,31 +11,55 @@ std::unordered_map<std::pair<unsigned int, unsigned int>, std::unordered_set<std
             continue;
         
         auto indexPair = std::make_pair(precondition->args.at(0), precondition->args.at(1));
-
-        auto pairs = std::unordered_set<std::pair<unsigned int, unsigned int>>();
-        auto indexFirsts = std::unordered_set<unsigned int>();
-        auto indexSecond = std::unordered_set<unsigned int>();
-        for (auto iter = candidateObjects->at(indexPair.first).begin(); iter != candidateObjects->at(indexPair.first).end(); iter++) {
-            for (auto iter2 = candidateObjects->at(indexPair.second).begin(); iter2 != candidateObjects->at(indexPair.second).end(); iter2++) {
-                auto pair = std::make_pair(*iter, *iter2);
-                if (state->ContainsFact(precondition->predicateIndex, pair) == precondition->value) {
-                    pairs.emplace(pair);
-                    indexFirsts.emplace(pair.first);
-                    indexSecond.emplace(pair.second);
-                }
-            }
-        }
-
-        if (!candidatePairs.contains(indexPair)) {
-            candidatePairs[indexPair] = pairs;
-            // Remove those from candidate objects which are not in any pairs
-            AlgorithmHelper::Intersect(candidateObjects->at(indexPair.first), indexFirsts);
-            AlgorithmHelper::Intersect(candidateObjects->at(indexPair.second), indexSecond);
-        } else {
-            // This happens if two preconditions have the same index pair, i.e. pre1: ?x, ?y & pre2: ?x, ?y
-            // In this case the legal pairs is the intersection between the two
-            AlgorithmHelper::Intersect(candidatePairs.at(indexPair), pairs);
-        }
+        auto legalPairInfo = GenerateLegalPairings(precondition, state, candidateObjects, indexPair);
+        UpdateCandidateObjects(indexPair, candidateObjects, &legalPairInfo);
+        UpdateLegalPairings(indexPair, &candidatePairs, &legalPairInfo);
     }
     return candidatePairs;
+}
+
+LegalPairInformation BinaryFactVerifier::GenerateLegalPairings( const PDDLLiteral* precondition,
+                                                                const PDDLState *state,
+                                                                const std::vector<std::unordered_set<unsigned int>> *candidateObjects,
+                                                                const std::pair<unsigned int, unsigned int> indexPair) 
+{
+    auto legalPairInformation = LegalPairInformation();
+
+    for (auto iter = candidateObjects->at(indexPair.first).begin(); iter != candidateObjects->at(indexPair.first).end(); iter++) {
+        for (auto iter2 = candidateObjects->at(indexPair.second).begin(); iter2 != candidateObjects->at(indexPair.second).end(); iter2++) {
+            auto pair = std::make_pair(*iter, *iter2);
+            if (state->ContainsFact(precondition->predicateIndex, pair) == precondition->value) {
+                legalPairInformation.pairs.emplace(pair);
+                legalPairInformation.indexFirst.emplace(pair.first);
+                legalPairInformation.indexSecond.emplace(pair.second);
+            }
+        }
+    }
+    return legalPairInformation;
+}
+
+void BinaryFactVerifier::UpdateCandidateObjects(const std::pair<unsigned int, unsigned int> indexPair, 
+                                                std::vector<std::unordered_set<unsigned int>> *candidateObjects,
+                                                const LegalPairInformation *newInformation) 
+{
+    // Remove those from candidate objects which are not in any pairs
+    AlgorithmHelper::Intersect(candidateObjects->at(indexPair.first), newInformation->indexFirst);
+    AlgorithmHelper::Intersect(candidateObjects->at(indexPair.second), newInformation->indexSecond);
+}
+
+void BinaryFactVerifier::UpdateLegalPairings(
+    const std::pair<unsigned int, unsigned int> indexPair,
+    std::unordered_map<std::pair<unsigned int, unsigned int>, std::unordered_set<std::pair<unsigned int, unsigned int>>> *candidatePairs,
+    const LegalPairInformation *newInformation) 
+{
+    if (!candidatePairs->contains(indexPair)) {
+        if (!candidatePairs->contains(indexPair))
+            candidatePairs->emplace(indexPair, newInformation->pairs);
+        else
+            candidatePairs->at(indexPair) = newInformation->pairs;
+    } else {
+        // This happens if two preconditions have the same index pair, i.e. pre1: ?x, ?y & pre2: ?x, ?y
+        // In this case the legal pairs is the intersection between the two
+        AlgorithmHelper::Intersect(candidatePairs->at(indexPair), newInformation->pairs);
+    }
 }
--- src/Walker/ActionGenerator/BinaryFactVerifier.hpp
+++ src/Walker/ActionGenerator/BinaryFactVerifier.hpp
@@ -9,6 +9,15 @@
 
 #include "../../Helpers/AlgorithmHelper.hh"
 
+struct LegalPairInformation{
+    // Set of legal pairings
+    std::unordered_set<std::pair<unsigned int, unsigned int>> pairs;
+    // Set of objects appearing in the left side of a legal pair
+    std::unordered_set<unsigned int> indexFirst;
+    // Set of objects appearing in the right side of a legal pair
+    std::unordered_set<unsigned int> indexSecond;
+};
+
 class BinaryFactVerifier {
 public:
     BinaryFactVerifier() {};
@@ -17,7 +26,17 @@ public:
         GetLegalPairings(   const PDDLAction* action, const PDDLState *state,
                             std::vector<std::unordered_set<unsigned int>> *candidateObjects);
 private:
-
+    static LegalPairInformation GenerateLegalPairings(  const PDDLLiteral* literal,
+                                                        const PDDLState *state,
+                                                        const std::vector<std::unordered_set<unsigned int>> *candidateObjects,
+                                                        const std::pair<unsigned int, unsigned int> indexPair);
+    static void UpdateCandidateObjects( const std::pair<unsigned int, unsigned int> indexPair, 
+                                        std::vector<std::unordered_set<unsigned int>> *candidateObjects,
+                                        const LegalPairInformation *newInformation);
+    static void UpdateLegalPairings(
+        const std::pair<unsigned int, unsigned int> indexPair,
+        std::unordered_map<std::pair<unsigned int, unsigned int>, std::unordered_set<std::pair<unsigned int, unsigned int>>> *candidatePairs,
+        const LegalPairInformation *newInformation);
 };
 
 #endif

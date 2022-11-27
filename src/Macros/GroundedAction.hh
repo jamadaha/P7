fefec5a063219c9aa77b8b7a47a2f42553ba7c49
Kristian Skov <kris701kj@gmail.com>
2022-11-27 16:04:43 +0100

    Renames

--- src/Macros/GroundedAction.hh
+++ src/Macros/GroundedAction.hh
@@ -10,17 +10,19 @@
 #include "../Helpers/Hashes.hh"
 #include "GroundedLiteral.hh"
 
-struct GroundedAction {
-    const std::string name;
-    const std::unordered_set<unsigned int> parameters;
-    const std::unordered_map<GroundedLiteral, bool> preconditions;
-    const std::unordered_map<GroundedLiteral, bool> effects;
-    GroundedAction(std::string name) : name(name) {};
-    GroundedAction(std::string name, 
-    std::unordered_set<unsigned int> parameters, 
-    std::unordered_map<GroundedLiteral, bool> preconditions,
-    std::unordered_map<GroundedLiteral, bool> effects) :
-    name(name), parameters(parameters), preconditions(preconditions), effects(effects) {};
-};
+namespace Macro {
+    struct GroundedAction {
+        const std::string name;
+        const std::unordered_set<unsigned int> parameters;
+        const std::unordered_map<GroundedLiteral, bool> preconditions;
+        const std::unordered_map<GroundedLiteral, bool> effects;
+        GroundedAction(std::string name) : name(name) {};
+        GroundedAction(std::string name,
+            std::unordered_set<unsigned int> parameters,
+            std::unordered_map<GroundedLiteral, bool> preconditions,
+            std::unordered_map<GroundedLiteral, bool> effects) :
+            name(name), parameters(parameters), preconditions(preconditions), effects(effects) {};
+    };
+}
 
 #endif
--- src/Macros/Macro.hh
+++ src/Macros/Macro.hh
@@ -6,15 +6,15 @@
 #include "GroundedAction.hh"
 #include "../IntermediatePDDL/PDDLInstance.hh"
 
-class Macro {
-public:
-    const std::string name;
-    const GroundedAction groundedAction;
-    const std::vector<PDDLActionInstance> path;
-    Macro(GroundedAction groundedAction, std::vector<PDDLActionInstance> path) : 
-        groundedAction(groundedAction), path(path), name("MACRO-" + groundedAction.name) {};
-private:
-    
-};
+namespace Macros {
+    class Macro {
+    public:
+        const std::string name;
+        const GroundedAction groundedAction;
+        const std::vector<PDDLActionInstance> path;
+        Macro(GroundedAction groundedAction, std::vector<PDDLActionInstance> path) :
+            groundedAction(groundedAction), path(path), name("MACRO-" + groundedAction.name) {};
+    };
+}
 
 #endif

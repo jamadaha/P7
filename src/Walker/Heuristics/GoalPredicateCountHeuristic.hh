3e5ffd0769d29d8e71e1e4ff2f1e2d9591fac031
Kristian Skov <kris701kj@gmail.com>
2022-12-02 10:35:49 +0100

    Cleaned up the class

--- src/Walker/Heuristics/GoalPredicateCountHeuristic.hh
+++ src/Walker/Heuristics/GoalPredicateCountHeuristic.hh
@@ -7,7 +7,6 @@
 class GoalPredicateCountHeuristic : public BaseHeuristic {
 public:
 	GoalPredicateCountHeuristic(const PDDL::Domain *domain, const PDDL::Problem *problem) : BaseHeuristic(domain, problem){
-		GenerateGoalPredicates();
 	};
 
 	PDDL::ActionInstance* NextChoice(PDDL::State * state, std::vector<PDDL::ActionInstance> *choices) override {
@@ -45,37 +44,6 @@ public:
 		return 0;
 	};
 private:
-	// The preconditions of actions which has an effect relating to goal state
-	// e.g. goal: ball at some room - actions: drop - preconditions: holding ball & at-room 
-	// Is a set as the case of same predicate different values is still valid
-	std::unordered_set<unsigned int> goalPreconditions;
-
-	void GenerateGoalPredicates() {
-		std::unordered_set<const PDDL::Action*> actions = GetRelevantActions();
-		for (auto action = actions.begin(); action != actions.end(); action++) {
-			for (int i = 0; i < (*action)->preconditions.size(); i++) {
-				const PDDL::Literal *literal = &(*action)->preconditions.at(i);
-				goalPreconditions.emplace(literal->predicateIndex);
-			}
-		}
-	}
-
-	// Retries those PDDL::Action whose effect relates to the goal state
-	// TODO: Handle cases where goal state requires something to be false
-	std::unordered_set<const PDDL::Action*> GetRelevantActions() {
-		std::unordered_set<const PDDL::Action*> actions;
-		for (int i = 0; i < domain->actions.size(); i++) {
-			const PDDL::Action *action = &domain->actions.at(i);
-			for (int eff = 0; eff < action->effects.size(); eff++) {
-				const PDDL::Literal *effect = &action->effects.at(eff);
-				bool containsEffectFact = (problem->goalState.unaryFacts.contains(effect->predicateIndex) && problem->goalState.unaryFacts.at(effect->predicateIndex).size() > 0);
-				if (effect->value && containsEffectFact)
-					actions.emplace(action);
-			}
-		}
-		return actions;
-	}
-
 	void Reset() override {};
 };
 
